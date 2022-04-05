7e05bce0d9494fe95182bef2b1633c94bb6b855e
Wilmara Ruiz <wilmara_andreina93@hotmail.com>
2022-04-05 08:03:27 -0500

    Correpcion de ejercicio

--- script.js
+++ script.js
@@ -1,46 +1,40 @@
+let captura = document.getElementById('form');
 
 const convertir = new Intl.NumberFormat('en-US', {
     style: 'currency',
     currency: 'USD',
     minimumFractionDigits: 0
-  })
+})
 
-    function captura(){
+captura.addEventListener("submit", (e) => {
+    e.preventDefault();
 
-        let valorCompra=document.getElementById("valorTotal").value ; //valorCompra ingresado en formulario
-    
-       let propina=document.getElementById("valorPropina").value ; //porcentaje de propina ingresado en formulario
-    
-        if  (!isNaN(valorCompra)){                            //se garantiza que en campo valorCompra se ingrese valores numericos
+    let valorCompra = document.getElementById("valorTotal").value; //valorCompra ingresado en formulario
 
-    
-            if (!isNaN(propina))  {                 //y en el en campo propinas se ingrese valores numericos
+    let propina = document.getElementById("valorPropina").value; //porcentaje de propina ingresado en formulario
 
-    
-                valorPropina=(valorCompra*propina)/100  //se aya valor monetario del % de propina    
+    if (!isNaN(valorCompra)) { //se garantiza que en campo valorCompra se ingrese valores numericos
 
-                total= parseInt( valorCompra) + parseInt(valorPropina)  //se gatantiza que se sumen los numeros parseInt
-                let subt = document.getElementById('p1');
-                subt.textContent=convertir.format(valorCompra);  
+        if (!isNaN(propina)) { //y en el en campo propinas se ingrese valores numericos
 
-                let subt1=document.getElementById('p2');
-                subt1.textContent=convertir.format(valorPropina);
+            valorPropina = (valorCompra * propina) / 100 //se aya valor monetario del % de propina    
 
-                let subt2=document.getElementById('p3');
-                subt2.textContent=convertir.format(total);;
+            total = parseInt(valorCompra) + parseInt(valorPropina) //se gatantiza que se sumen los numeros parseInt
+
+            let subt = document.getElementById('p1');
+            subt.textContent = convertir.format(valorCompra);
+
+            let subt1 = document.getElementById('p2');
+            subt1.textContent = convertir.format(valorPropina);
+
+            let subt2 = document.getElementById('p3');
+            subt2.textContent = convertir.format(total);;
+
+            document.getElementById("valorTotal").value = "";
+            document.getElementById("valorPropina").value = "";
         }
+    } else {
+        alert(`Ingrese valor valido: "Digitos"`)
+
     }
-        else{
-            alert(`Ingrese valor valido: "Digitos"`)
-            
-        }
-    
-    
-   }
-
-   
- 
-
-
-    
-
+})
